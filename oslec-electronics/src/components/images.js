ce4323bf969080fb7b6b1e1aa84709f76de2d832
Jeran <109207378+JeranPD@users.noreply.github.com>
2023-03-13 15:28:21 +0800

    Delete oslec-electronics directory

--- oslec-electronics/src/components/images.js
+++
@@ -1,8 +0,0 @@
-import image1 from '../assets/image/jobDone/image1.jpg';
-import image2 from '../assets/image/jobDone/image2.jpg';
-import image3 from '../assets/image/jobDone/image3.jpg';
-import image4 from '../assets/image/jobDone/image4.jpg';
-import image5 from '../assets/image/jobDone/image5.jpg';
-import image6 from '../assets/image/jobDone/image6.jpg';
-
-export default [image1, image2, image3, image4, image5, image6]
