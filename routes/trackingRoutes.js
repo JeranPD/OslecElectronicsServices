c5c93f29a80a0ba70393006afea489bf104cf743
Jeran <109207378+JeranPD@users.noreply.github.com>
2023-03-13 15:28:28 +0800

    Delete routes directory

--- routes/trackingRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-
-import { trackStatus } from "../controllers/trackingController.js";
-
-router.route("/").get(trackStatus);
-
-export default router;
