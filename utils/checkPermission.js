ba59f0d5a31d77da8e75103bca11dc5ccec19263
Jeran <109207378+JeranPD@users.noreply.github.com>
2023-03-13 15:28:34 +0800

    Delete utils directory

--- utils/checkPermission.js
+++
@@ -1,9 +0,0 @@
-import { UnAuthenticatedError } from "../errors/index.js";
-
-const checkPermission = (requestAdmin, resourceAdminId) => {
-  if (requestAdmin.id === resourceAdminId.toString()) return;
-
-  throw new UnAuthenticatedError("Not authorized to access this route");
-};
-
-export default checkPermission;
